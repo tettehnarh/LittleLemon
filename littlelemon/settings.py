4612f149b2b3f71fd80c80271ebc72759d0d8c5d
tettehnarh <leslienarh@outlook.com>
2024-04-25 10:30:27 +0000

    db configuration and model setup

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': '210187HarLES',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
